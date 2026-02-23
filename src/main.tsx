4118b08aaf45126ce01bff40d0f4e13c9f75a684
raajnarayan2318 <raajnarayan.vgf287@gmail.com>
2026-02-23 16:05:05 -0600

    Fix routing for GitHub Pages using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./app/App";
 import "./index.css";
 import "./styles/cursor.css";
 
+const Router =
+  import.meta.env.MODE === "ghpages" ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
